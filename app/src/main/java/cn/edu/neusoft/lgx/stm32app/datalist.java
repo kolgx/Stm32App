1d57d16c969ae89d630f80389d510d1f5fe83a2b
kolgx <2547525908@qq.com>
2021-10-07 17:34:14 +0800

    Initial commit

---
+++ app/src/main/java/cn/edu/neusoft/lgx/stm32app/datalist.java
@@ -0,0 +1,9 @@
+package cn.edu.neusoft.lgx.stm32app;
+
+public class datalist {
+    public String num;
+    public int tem;
+    public int hum;
+    public String fire;
+    public String worn;
+}
